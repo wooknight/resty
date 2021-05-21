2291274e7c2924af2ce8c50fa2ccd93a34eb454a
Ramesh Naidu <rameshnaidu@gmail.com>
2021-05-20 21:49:07 -0700

    removed template

--- chapter1/chapter1.go
+++ chapter1/chapter1.go
@@ -6,8 +6,6 @@ import (
 	"log"
 	"net/http"
 	"time"
-
-	"html/template"
 )
 
 type Todo struct {
@@ -64,25 +62,25 @@ func main() {
 	})
 	http.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
 		// io.WriteString(w, "Hello from /users")
-		http.Error(w, "fucked", http.StatusNotFound)
+		http.Error(w, "testing", http.StatusNotFound)
 	})
 	http.Handle("/bhosada", SecretTokenHandler{
 		next:   NewUptimeHandler(),
 		secret: "mysecret",
 	})
 
-	tmpl := template.Must(template.ParseFiles("layout.html"))
-	http.HandleFunc("/template", func(w http.ResponseWriter, r *http.Request) {
-		data := TodoPageData{
-			PageTitle: "My TODO list",
-			Todos: []Todo{
-				{Title: "Task 1", Done: false},
-				{Title: "Task 2", Done: true},
-				{Title: "Task 3", Done: true},
-			},
-		}
-		tmpl.Execute(w, data)
-	})
+	// tmpl := template.Must(template.ParseFiles("layout.html"))
+	// http.HandleFunc("/template", func(w http.ResponseWriter, r *http.Request) {
+	// 	data := TodoPageData{
+	// 		PageTitle: "My TODO list",
+	// 		Todos: []Todo{
+	// 			{Title: "Task 1", Done: false},
+	// 			{Title: "Task 2", Done: true},
+	// 			{Title: "Task 3", Done: true},
+	// 		},
+	// 	}
+	// 	tmpl.Execute(w, data)
+	// })
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
