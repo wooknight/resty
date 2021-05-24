cf47278f8dd734dc1393e1b1b336f1a93b348ece
Ramesh Naidu <rameshnaidu@gmail.com>
2021-05-23 21:49:36 -0700

    REST KV

--- kv/main.go
+++ kv/main.go
@@ -2,16 +2,41 @@ package main
 
 import (
 	"fmt"
-
 	"kv/data"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
+
+func helloMuxHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Gorilla Mux\n"))
+}
+
+func friskMuxHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello Frisky from Gorilla Mux\n"))
+}
+func businessMuxHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["key"]
+	val, _ := data.Get(key)
+	w.Write([]byte("Gorilla muxh business" + val + "\n"))
+}
+
+func ratingListMuxHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Pictures \n Videos \n Text \n"))
+}
+func ratingMuxHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello Business from Gorilla Mux\n"))
+}
 
 func main() {
 	fmt.Println("Here we go")
-	data.Put("testy", "pav")
-	val, ok := data.Get("testy")
-	fmt.Println("Value for testy is ", val, ok)
-	data.Delete("testy")
-	val, ok = data.Get("testy")
-	fmt.Println("Value for testy is ", val, ok)
+	data.Put("121", "pav")
+	r := mux.NewRouter()
+	r.HandleFunc("/", helloMuxHandler)
+	r.HandleFunc("/business/{key}", businessMuxHandler)
+	r.HandleFunc("/user/rating/{type}", ratingListMuxHandler)
+	r.HandleFunc("/user/rating/{id:[0-9]+}", ratingMuxHandler)
+	log.Fatal(http.ListenAndServe(":8081", r))
 }
