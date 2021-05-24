4724d311e990ace60795337696151117b24f38b3
Ramesh Naidu <rameshnaidu@gmail.com>
2021-05-24 07:54:32 -0700

    Added GET and PUT

--- kv/main.go
+++ kv/main.go
@@ -5,7 +5,7 @@ import (
 	"kv/data"
 	"log"
 	"net/http"
-
+	"io"
 	"github.com/gorilla/mux"
 )
 
@@ -22,20 +22,46 @@ func businessMuxHandler(w http.ResponseWriter, r *http.Request) {
 	val, _ := data.Get(key)
 	w.Write([]byte("Gorilla muxh business" + val + "\n"))
 }
+func businessCreateMuxHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["key"]
+	value,err := io.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		http.Error(w,err.Error(),http.StatusInternalServerError)
+		return
+	}
+	err = data.Put(key,(string)(value))
+	if err != nil {
+		http.Error(w,err.Error(),http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+}
+
 
 func ratingListMuxHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Pictures \n Videos \n Text \n"))
+	vars := mux.Vars(r)
+	fmt.Printf("%v\n", vars)
+	key := vars["type"]
+	val, _ := data.Get(key)
+	w.Write([]byte("Pictures \n Videos \n Text \n" + val))
 }
 func ratingMuxHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello Business from Gorilla Mux\n"))
+	vars := mux.Vars(r)
+	key := vars["id"]
+	val, _ := data.Get(key)
+	w.Write([]byte("Hello Business from Gorilla Mux\n" + val))
 }
 
 func main() {
 	fmt.Println("Here we go")
 	data.Put("121", "pav")
+	data.Put("video", "pucchi")
 	r := mux.NewRouter()
 	r.HandleFunc("/", helloMuxHandler)
-	r.HandleFunc("/business/{key}", businessMuxHandler)
+	r.HandleFunc("/business/{key}", businessMuxHandler).Methods("GET")
+	r.HandleFunc("/business/{key}", businessCreateMuxHandler).Methods("PUT")
 	r.HandleFunc("/user/rating/{type}", ratingListMuxHandler)
 	r.HandleFunc("/user/rating/{id:[0-9]+}", ratingMuxHandler)
 	log.Fatal(http.ListenAndServe(":8081", r))
